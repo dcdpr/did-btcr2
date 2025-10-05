22dd42ffc5ba25d2a8e66d70efd05d12703a7ec7
Jay Oster <jay@blipjoy.com>
2025-10-05 15:32:59 -0700

    Integrate pagetoc into the normal TOC sidebar
    The terminology and algorithms chapters are the longest. They make the
    best test cases for headers getting mapped into the chapters list.
    
    The mdbook-pagetoc preprocessor uses JavaScript to insert the header
    links. I don't know why. So, ok, it injects the TOC on the client
    side. Fine. But at least use normal HTML lists and CSS to represent
    the structure!
    
    The CSS has been completely replaced with just a simple "dimming" filter.
    Otherwise uses the existing sidebar CSS.
    
    It is also possible to make the headers collapsible in the TOC. (This is
    done by `[output.html.fold]`.) But I haven't really explored that option
    yet.

--- mdbook/theme/pagetoc.js
+++ mdbook/theme/pagetoc.js
@@ -1,7 +1,7 @@
 let scrollTimeout;
 
-const listenActive = () => {
-  const elems = document.querySelector(".pagetoc").children;
+function listenActive() {
+  const elems = document.querySelectorAll(".pagetoc li > a");
   [...elems].forEach(el => {
     el.addEventListener("click", (event) => {
       clearTimeout(scrollTimeout);
@@ -17,17 +17,22 @@ const listenActive = () => {
 
 const getPagetoc = () => document.querySelector(".pagetoc") || autoCreatePagetoc();
 
-const autoCreatePagetoc = () => {
-  const main = document.querySelector("#content > main");
-  const content = Object.assign(document.createElement("div"), {
-    className: "content-wrap"
-  });
-  content.append(...main.childNodes);
-  main.prepend(content);
-  main.insertAdjacentHTML("afterbegin", '<div class="sidetoc"><nav class="pagetoc"></nav></div>');
+function autoCreatePagetoc() {
+  const active_li = document.querySelector("mdbook-sidebar-scrollbox ol > li:has(> a.active)");
+  if (active_li) {
+    active_li.insertAdjacentHTML("afterend", '<li class="expanded"><ol class="pagetoc section"></ol></li>');
+  }
+
   return document.querySelector(".pagetoc");
 };
-const updateFunction = () => {
+
+function getChapterNumber() {
+  const chapter_label_number = document.querySelector("mdbook-sidebar-scrollbox ol > li > a.active > strong");
+
+  return chapter_label_number.textContent;
+}
+
+function updateFunction() {
   if (scrollTimeout) return; // Skip updates if within the cooldown period from a click
   const headers = [...document.getElementsByClassName("header")];
   const scrolledY = window.scrollY;
@@ -41,7 +46,7 @@ const updateFunction = () => {
     }
   }
 
-  const pagetocLinks = [...document.querySelector(".pagetoc").children];
+  const pagetocLinks = [...document.querySelectorAll(".pagetoc li > a")];
   pagetocLinks.forEach(link => link.classList.remove("active"));
 
   if (lastHeader) {
@@ -50,20 +55,76 @@ const updateFunction = () => {
   }
 };
 
-window.addEventListener('load', () => {
-  const pagetoc = getPagetoc();
-  const headers = [...document.getElementsByClassName("header")];
+function headersIntoTree(headers) {
+  const tree = [];
+  let last_level = 1;
+  let last_section = null;
+
   headers.forEach(header => {
-    const parent = header.parentElement
-    if (!parent.classList.contains("toc-ignore")) {
-      const link = Object.assign(document.createElement("a"), {
-        textContent: [...parent.childNodes].map(({ textContent }) => textContent).join(''),
-        href: header.href,
-        className: `pagetoc-${parent.tagName}`
-      });
-      pagetoc.appendChild(link);
+    const level = parseInt(header.parentElement.tagName.substring(1) - 1);
+    if (level > 0) {
+      // TODO: This logic isn't correct. It doesn't create a tree.
+      if (level != last_level) {
+        if (last_section) {
+          tree.push(last_section);
+        }
+        last_level = level;
+        last_section = [];
+      }
+
+      (last_section || tree).push(header);
     }
   });
+
+  if (last_section) {
+    tree.push(last_section);
+  }
+
+  return tree;
+}
+
+function createSection(list_root, chapter_number, tree) {
+  tree.forEach((header, i) => {
+    if (Array.isArray(header)) {
+      const list_item = Object.assign(document.createElement("li"), {
+        className: "chapter-item expanded",
+      });
+      const ordered_list = Object.assign(document.createElement("ol"), {
+        className: "section",
+      });
+      list_item.appendChild(ordered_list);
+      createSection(ordered_list, `${chapter_number}${i}.`, header);
+      list_root.appendChild(list_item);
+    } else {
+      console.log("header: ", header);
+      const parent = header.parentElement
+      if (!parent.classList.contains("toc-ignore")) {
+        const list_item = Object.assign(document.createElement("li"), {
+          className: "chapter-item expanded",
+        });
+        const section_number = Object.assign(document.createElement("strong"), {
+          textContent: `${chapter_number}${i + 1}. `,
+          ariaHidden: "true",
+        });
+        const link = Object.assign(document.createElement("a"), {
+          textContent: [...parent.childNodes].map(({ textContent }) => textContent).join(''),
+          href: header.href,
+        });
+        link.insertAdjacentElement("afterbegin", section_number);
+        list_item.appendChild(link);
+        list_root.appendChild(list_item);
+      }
+    }
+  });
+}
+
+window.addEventListener('load', () => {
+  const pagetoc = getPagetoc();
+  if (!pagetoc) return;
+  const chapter_number = getChapterNumber();
+  const tree = headersIntoTree([...document.getElementsByClassName("header")]);
+  createSection(pagetoc, chapter_number, tree);
+
   updateFunction();
   listenActive();
   window.addEventListener("scroll", updateFunction);
